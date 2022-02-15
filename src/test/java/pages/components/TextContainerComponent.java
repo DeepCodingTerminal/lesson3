a4a1862d9a405cda5b19ff8e0d8592d2663553e8
Testorium <pmichael909@gmail.com>
2022-02-15 23:12:41 +0300

    update

--- src/test/java/pages/components/TextContainerComponent.java
+++
@@ -1,9 +0,0 @@
-package pages.components;
-
-import static com.codeborne.selenide.Selenide.$;
-
-public class TextContainerComponent {
-    public void insertValue(String locator, String text) {
-        $(locator).setValue(text).pressEnter();
-    }
-}
--- src/test/java/pages/components/UploadFileComponent.java
+++
@@ -1,9 +0,0 @@
-package pages.components;
-
-import static com.codeborne.selenide.Selenide.$;
-
-public class UploadFileComponent {
-    public void uploadFile(String locator, String fileName) {
-        $(locator).uploadFromClasspath(fileName);
-    }
-}
